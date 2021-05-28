40d37c825afff52e995ba484e54ae395e627eb27
koki kanasaka <k.koki980701@gmail.com>
2021-05-28 12:43:08 +0900

    新規タスクを登録する機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,15 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
 
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  private
+  task_params
+    params.require(:task).permit(:task_name, :task_detail)
+  end
 end
