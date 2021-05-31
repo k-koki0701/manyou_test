1e40c0e1a0808444a1dca57f4ace16fc2dcb1fad
koki kanasaka <k.koki980701@gmail.com>
2021-05-31 14:39:45 +0900

    終了期限を登録できる機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task_name, :task_detail)
+    params.require(:task).permit(:task_name, :task_detail, :end_period)
   end
 
   def set_task
