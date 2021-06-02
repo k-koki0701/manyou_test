d079e806d2eca4732b5c12b855d4a0c59122d849
koki kanasaka <k.koki980701@gmail.com>
2021-06-02 23:03:57 +0900

    taskとuserを紐付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,5 @@ class Task < ApplicationRecord
   scope :search_task_name, ->(task_name) { where(['task_name LIKE ?', task_name]) }
   scope :search_status, ->(status) { where(status: status) }
   enum priority: { 高: 0, 中: 1, 低: 2 }
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_133654) do
+ActiveRecord::Schema.define(version: 2021_06_02_140003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2021_06_02_133654) do
     t.datetime "end_period", default: -> { "now()" }, null: false
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["task_name"], name: "index_tasks_on_task_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_06_02_133654) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
