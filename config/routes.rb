f15827b3d5c87918958bb6f04b1d34d1bf4efb07
koki kanasaka <k.koki980701@gmail.com>
2021-06-03 15:57:23 +0900

    namespace を使いグループ化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   resources :tasks
-  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  namespace :admin do
+    resources :users, only: [:new, :create, :show]
+  end
 end
