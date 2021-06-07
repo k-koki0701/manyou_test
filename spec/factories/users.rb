02c465611de97efb10b3ea0439f61510af8568ff
koki kanasaka <k.koki980701@gmail.com>
2021-06-07 15:18:11 +0900

    ユーザテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "user" }
+    sequence(:email) { "user@example.com" }
+    password { "user_password" }
+    admin { 'false' }
+  end
+  factory :second_user, class: User do
+    name { "admin_user" }
+    sequence(:email) { "admin@example.com" }
+    password { "admin_password" }
+    admin { 'true' }
   end
 end
